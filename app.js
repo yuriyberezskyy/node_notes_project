69a0a3b16238b96a91d1fd82f84c40d114e875e0
yuriyberezskyy <bereza95@gmail.com>
2020-07-07 22:34:12 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// const add = require("./utils");
+
+// const sum = add(4, -2);
+// console.log(sum);
+
+const notes = require("./notes");
+
+console.log(notes());
---
+++ utils.js
@@ -0,0 +1,8 @@
+console.log("Utils.js");
+
+const name = "Mike";
+const add = function (a, b) {
+  return a + b;
+};
+
+module.exports = add;
