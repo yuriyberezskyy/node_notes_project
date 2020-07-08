5b5bb78af5dece48c523a519928105f1ad055c85
yuriyberezskyy <bereza95@gmail.com>
2020-07-08 00:44:23 -0400

    downloaded nodemon to run code after each savings

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
-
 const getNotes = require("./notes");
+
 const msg = getNotes();
 console.log(msg);
-console.log(chalk.blue.bgYellow("Hello world!"));
+console.log(chalk.blue.bold.inverse("Error!"));
