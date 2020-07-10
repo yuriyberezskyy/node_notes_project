37683da31fe5b39590dc4f8b75503a205cc8809c
yuriyberezskyy <bereza95@gmail.com>
2020-07-10 18:00:24 -0400

    command add and remove in process.argv

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const chalk = require("chalk");
 const getNotes = require("./notes");
 
-const msg = getNotes();
-console.log(msg);
-console.log(chalk.blue.bold.inverse("Error!"));
+const command = process.argv[2];
+console.log(process.argv);
+
+if (command === "add") {
+  console.log("Adding note!");
+} else if (command === "remove") {
+  console.log("Removing note!");
+}
