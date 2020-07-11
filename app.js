3a8351290ce7670313f05e5e5888820fdb2661b8
yuriyberezskyy <bereza95@gmail.com>
2020-07-10 20:16:55 -0400

    done

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const chalk = require("chalk");
 const yargs = require("yargs");
-const getNotes = require("./notes");
-const { string } = require("yargs");
+const notes = require("./notes");
 
 // Customize yargs version
 yargs.version("1.1.0");
@@ -23,7 +22,7 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log("Title: " + argv.title, "Body: " + argv.body);
+    notes.addNote(argv.title, argv.body);
   },
 });
 
