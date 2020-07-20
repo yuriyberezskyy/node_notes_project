d35f25da9ae6d72557cc5e0bae6c4f2d162f9f9b
yuriyberezskyy <bereza95@gmail.com>
2020-07-20 17:48:48 -0400

    transform in arrow function"

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ yargs.command({
       type: "string",
     },
   },
-  handler: function (argv) {
+  handler(argv) {
     notes.addNote(argv.title, argv.body);
   },
 });
@@ -35,7 +35,7 @@ yargs.command({
       type: "string",
     },
   },
-  handler: function (argv) {
+  handler(argv) {
     notes.removeNote(argv.title);
   },
 });
@@ -43,7 +43,7 @@ yargs.command({
 yargs.command({
   command: "list",
   describe: "List your notes",
-  handler: function () {
+  handler() {
     console.log("Listing out all notes");
   },
 });
@@ -51,7 +51,7 @@ yargs.command({
 yargs.command({
   command: "read",
   describe: "Read a note",
-  handler: function () {
+  handler() {
     console.log("Reading the note");
   },
 });
